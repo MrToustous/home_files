2334f1b377ee7225702e90403739f0092842064f
Toussaint <toussaint.behaghel@gmail.com>
2020-02-01 19:14:35 +0100

    feat: new install bash script

---
+++ new_install.sh
@@ -0,0 +1,7 @@
+sudo apt install vim --assume-yes
+sudo apt install htop --assume-yes
+sudo apt install i3 --assume-yes
+sudo apt install git --assume-yes
+cp .bashrc ~/
+cp .vimrc ~/
+mkdir ~/.config/i3/ && cp .config/i3/config ~/.config/i3/config
