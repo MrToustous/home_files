7c0526fce1d1a5ee60b801e66ed3fa9f2ed9208e
Toussaint <t.behaghel@sunzulab.com>
2022-10-03 11:05:35 +0200

    only aliases in bashrc and upsate new install file

--- new_install.sh
+++ new_install.sh
@@ -2,6 +2,6 @@ sudo apt install vim --assume-yes
 sudo apt install htop --assume-yes
 sudo apt install i3 --assume-yes
 sudo apt install git --assume-yes
-cp .bashrc ~/
-cp .vimrc ~/
+# cp .bashrc ~/
+# cp .vimrc ~/
 mkdir ~/.config/i3/ && cp .config/i3/config ~/.config/i3/config
